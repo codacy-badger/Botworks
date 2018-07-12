5f993c6570ff3bccd4b02748cc83a007c0604391
Aaron Shannon-Honson <aaron.shannon-honson@adelaide.edu.au>
2018-07-12 11:40:32 +0930

    Typing time calculation changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 var currentPath = "default";
 var botui = new BotUI('bot');
 
+//construction of say() method
 function say(x, index) {
   if (x.paths[currentPath] != undefined) { //if the chosen path exists, do this...
     if (index <= x.paths[currentPath].messages.length - 1) {
       var message = x.paths[currentPath].messages[index];
       if (message.bot == true) {
+        var typingTime = message.text.length * 15;
+        typingTime *= Math.random() * 1.5 + 0.5;
         botui.message.add({
-          delay: 3000,
+          delay: typingTime, //calculated by length of the message +/- some random amount, to simulate "real" typing times for immersion
           authorImg: message.authorImg,
           imgSrc: "./img/testAv.jpg",
           loading: true,
@@ -16,10 +19,9 @@ function say(x, index) {
         setTimeout(function() {
           index++;
           say(x, index);
-        }, 3000);
+        }, typingTime);
       } else { //TODO: add handlers for text entry as well as buttons
         botui.action.button({
-          
           action: message.choices
         }).then(function(res) { // will be called when a button is clicked.
           botui.action.hide();
@@ -41,5 +43,9 @@ function say(x, index) {
     })
   }
 }
+//END say()
 
-say(c, 0);
+
+
+
+say(c, 0); //say() method, calling stored JSON var
